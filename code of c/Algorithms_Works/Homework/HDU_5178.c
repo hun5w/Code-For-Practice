b08710238c22fb0ca9526f02c0ba4bc0b7ac4b0f
hun5w <1073613765@qq.com>
2024-09-13 00:23:24 +0800

    Add solution for problem HDU_5178 in Algorithms_Works/Homework

--- code of c/Algorithms_Works/Homework/HDU_5178.c
+++ code of c/Algorithms_Works/Homework/HDU_5178.c
@@ -10,3 +10,37 @@ Next n lines contain an integer x[i](−109≤x[i]≤109), means the X coordinat
 
 Output
 For each case, output an integer means how many pairs<a,b> that |x[b]−x[a]|≤k.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int n, k;
+        scanf("%d %d", &n, &k);
+        int x[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &x[i]);
+        }
+
+        qsort(x, n, sizeof(int), compare);
+
+        long long count = 0;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j < n && x[j] - x[i] <= k) {
+                j++;
+            }
+            count += (j - i - 1);
+        }
+
+        printf("%lld\n", count);
+    }
+    return 0;
+}
