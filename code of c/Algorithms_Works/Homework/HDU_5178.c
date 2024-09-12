395a65e0ba1fd515719fa1d3eb6f9c714382664e
hun5w <1073613765@qq.com>
2024-09-13 00:01:20 +0800

    Add solution for problem HDU_5178 in Algorithms_Works/Homework

---
+++ code of c/Algorithms_Works/Homework/HDU_5178.c
@@ -0,0 +1,12 @@
+/*Problem Description
+John has n points on the X axis, and their coordinates are (x[i],0),(i=0,1,2,…,n−1). He wants to know how many pairs<a,b> that |x[b]−x[a]|≤k.(a<b)
+ 
+
+Input
+The first line contains a single integer T (about 5), indicating the number of cases.
+Each test case begins with two integers n,k(1≤n≤100000,1≤k≤109).
+Next n lines contain an integer x[i](−109≤x[i]≤109), means the X coordinates.
+ 
+
+Output
+For each case, output an integer means how many pairs<a,b> that |x[b]−x[a]|≤k.*/
