21160af4e9bb2f486c36719c5efc8f3aecc6c596
hun5w <1076813765@qq.com>
2024-09-13 11:43:38 +0800

    python第二周课后作业初稿

--- code of python/pythonProject/python/Week2/work_02.py
+++ code of python/pythonProject/python/Week2/work_02.py
@@ -1,2 +1,17 @@
 """第二周周五课堂作业"""
-'print'
+'''
+课堂练习
+path=r"C:\Users\86189\Desktop\Code\Code-For-Practice"
+print(path,end="\n")
+
+mypath="python"
+print(mypath[1:3])
+print(mypath[:-1])
+'''
+
+#coding=utf-8
+import random as rm
+
+xin="张唐彭徐魏"
+ming1="家宇能百志"
+ming2="祺"
