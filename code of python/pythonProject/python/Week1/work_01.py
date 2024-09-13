269a03b2e05fc16fa46640df9bf8fa6d25881e00
hun5w <1076813765@qq.com>
2024-09-13 10:21:10 +0800

    补足了第一周未完成的python作业

--- code of python/pythonProject/python/Week1/work_01.py
+++ code of python/pythonProject/python/Week1/work_01.py
@@ -1,4 +1,4 @@
-import random as rm;
+import random as rm
 
 def getmatrix(n):
     '''
@@ -7,4 +7,23 @@ def getmatrix(n):
     :return:
     '''
 
-    mat = [[0 for i in range(n)]]
+    mat = [[0 for i in range(n)]for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            mat[i][j] = rm.randint(1,10)
+    return mat
+
+def print_matrix(mat):
+    '''
+
+    :param mat:
+    :return:
+    '''
+    m=len(mat)
+    for i in range(m):
+        print(mat[i])
+
+if __name__=="__main__":
+    n=int(input("请输入n生成一个n*n的矩阵\n"))
+    matl=getmatrix(n)
+    print_matrix(matl)
