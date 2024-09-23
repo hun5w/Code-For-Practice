e2bda64e6a737d26bc6c9933cbac746a2b054395
hun5w <1076813765@qq.com>
2024-09-23 22:45:35 +0800

    python_homework_week4

---
+++ code of python/pythonProject/DateAnalysis/week4_1.py
@@ -0,0 +1,22 @@
+
+'''
+name = input('name:')
+request = input("phone_numble(p) or address(a) or email(e)")
+if request == 'p':
+    key = 'tel'
+if request == 'a'
+    key = 'address'
+if request == 'e'
+    key = ('email')
+'''
+
+print('practice_1')
+age = int(input('请输入年龄 '))
+if age >= 18:
+    print("yes")
+else:
+    print('no')
+
+print('practice_2')
+name = input('请输入姓名')
+inf = input('')
