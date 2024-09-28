04fcca88c65f377cf0c9eac98b3d8e1dc5a1f9a6
hun5w <1076813765@qq.com>
2024-09-28 23:20:20 +0800

    算法作业

--- code of c/Algorithms_Works/Homework/Work2/HDU_4004.c
+++ code of c/Algorithms_Works/Homework/Work2/HDU_4004.c
@@ -16,13 +16,53 @@ Then n lines follow. Each stands for the distance from the starting banks to the
 #include<stdlib.h>
 #include<math.h>
 
+//判断是否能跳过
 int canCross(int *stones, int n, int L, double m, int maxJump){
     int jumps = 0, lastPostion = 0;
     for(int i = 0; i< n; i++){
         if(stones[i] - lastPostion > maxJump){
             jumps++;
             lastPostion = stones[i-1];
-            if()
+            if(stones[i]-lastPostion > maxJump){
+                return 0;
+            }
         }
     }
+    jumps++;
+    return jumps <= m; //返回是否能跳过
+}
+
+//比较函数，用于qsort排序
+int compare(const void *a,const void *b){
+    return *(int *)a - *(int *)b;
+}
+
+int main(){
+    int T;
+    scanf("%d", &T); 
+
+    while(T--){
+    int L, n, m; //L为河的长度，n为石头的数量，m为最多跳的次数
+    while(scanf("%d %d %d", &L, &n, &m)!=EOF){
+        int stones[n+1];//石头的位置
+        for(int i = 0; i < n; i++){
+            scanf("%d", &stones[i]); //输入石头的位置
+        }
+        stones[n] = L; //最后一个石头的位置为河的长度
+        qsort(stones, n, sizeof(int), compare); //排序
+
+        int low = 1, high = L, result = L; //二分查找
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(canCross(stones, n, L, m, mid)){
+                result = mid;
+                high = mid -1;
+        }else{
+            low = mid +1;
+            }
+        }
+            printf("%d\n", result);//输出当前最大跳跃距离    
+    }
+    }
+    return 0;
 }
